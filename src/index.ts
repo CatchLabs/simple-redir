e002f0f67b090bf943209699ba04b3c9c3835978
Zeno Zeng <zenoofzeng@gmail.com>
2016-08-11 17:07:04 +0800

    header for pkg.version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import * as Koa from 'koa';
 const app = new Koa();
+const pkg = require('../package');
+
+app.use(async (ctx) => {
+    ctx.set('X-Redirect-Service-Version', pkg.version);
+});
+
 app.listen(process.env.PORT || 3020);
