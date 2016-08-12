f6b19bf071542c9ad1d1c1080ec603da65721aa1
Zeno Zeng <zenoofzeng@gmail.com>
2016-08-12 19:54:29 +0800

    CreateReference and DeleteReference

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import * as Router from 'koa-router';
 import models from '../models';
+import * as uuid from 'node-uuid';
 const router = new Router();
 
 async function redirect(ctx: Router.IRouterContext) {
@@ -9,7 +10,7 @@ async function redirect(ctx: Router.IRouterContext) {
         ctx.status = 404;
         return;
     }
-    let ref = JSON.parse(link.references).filter((ref: {url: string, userAgent: string}) => {
+    let ref = JSON.parse(link.references || '[]').filter((ref: {url: string, userAgent: string}) => {
         if (!ref.userAgent) {
             return true;
         } else {
@@ -38,22 +39,22 @@ router.get('/:token/:param', redirect);
 router.get('/', async (ctx) => {
     const links = await models.link.findAll();
     ctx.type = '.json';
-    ctx.body = JSON.stringify(links.map((link) => (link.references = JSON.parse(link.references)) && link));
+    ctx.body = JSON.stringify(links.map((link) => (link.references = JSON.parse(link.references || '[]')) && link));
 });
 
 /**
  * @api {PUT} /:token Create or update link
  * @apiName PutLink
  * @apiGroup Link
- * @apiParam {String} url Target url
  * @apiParam {String} token
+ * @apiParam {Array} references
  */
 router.put('/:token', async (ctx: any) => {
     const {token} = ctx.params;
     const {body} = ctx.request;
     const [link, created] = await models.link.findOrCreate({ where: { token } });
-    if (body.url) {
-        link.set('references', JSON.stringify([ {url: body.url} ]));
+    if (body.references) {
+        link.set('references', JSON.stringify(body.references));
     }
     if (body.token) {
         link.set('token', body.token);
@@ -75,7 +76,42 @@ router.delete('/:token', async (ctx) => {
     } else {
         await link.destroy();
     }
-    ctx.body = 'OK';
-})
+    ctx.status = 200;
+});
+
+/**
+ * @api {POST} /:token/references Create reference
+ * @apiName CreateReference
+ * @apiGroup Reference
+ */
+router.post('/:token/references', async (ctx: any) => {
+    const {token} = ctx.params;
+    const {url, userAgent} = ctx.request.body;
+    const [link, created] = await models.link.findOrCreate({ where: { token } });
+    const id = uuid.v4();
+    let references = JSON.parse(link.references || '[]');
+    references.push({id, userAgent, url});
+    link.set('references', JSON.stringify(references));
+    await link.save();
+    ctx.status = 200;
+});
+
+/**
+ * @api {DELETE} /:token/references/:refId Delete reference
+ * @apiName DeleteReference
+ * @apiGroup Reference
+ */
+router.delete('/:token/references/:refId', async (ctx) => {
+    const {token, refId} = ctx.params;
+    const link = await models.link.findOne({ where: { token } });
+    if (!link) {
+        ctx.status = 404;
+        return;
+    }
+    let references = JSON.parse(link.references || '[]').filter((ref: {id: string}) => ref.id != refId);
+    link.set('references', JSON.stringify(references));
+    await link.save();
+    ctx.status = 200;
+});
 
 export default router;
